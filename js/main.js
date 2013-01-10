22fcc2ba85bd8673ffdaf17cea0f303dd8afc163
Iacuzzo Giovanni <iacuzzogiovanni@gmail.com>
2013-01-10 01:11:09 +0100

    listage saison et episode

--- js/main.js
+++ js/main.js
@@ -22,7 +22,10 @@
 	var $toAdd = $("#toAdd");
 	var oIfByClick = {"type": undefined};
 	var $searchButton = $(".icon-search");
-
+	var $infosBox = $("#infosSerie");
+	var $maSerie = $("#maSerie");
+	var $toPlan = $("#toPlan");
+	
 	// --- methods
 	var checkIfAlreadyLogIn = function(){
 		if(window.localStorage.length == 0){
@@ -56,6 +59,7 @@
 
 	var listSeries = function(e){
 		$searchButton.hide();
+		$searchBox.hide();
 		$listeDesSeries.empty();
 		iCurrentPage = 1;
 		currentPageName();
@@ -66,7 +70,7 @@
 		for( var serie in window.localStorage ){
 			if( serie.substring( 0, 6 ) === "serie_" ){
 				var dataSerie = JSON.parse(window.localStorage.getItem(serie));
-				$listeDesSeries.append('<li class="serie">' + dataSerie.title + '</li>');				
+				$listeDesSeries.append('<li class="serie" data-url="' + dataSerie.url +'"><span>' + dataSerie.title + '</span><button class="icon-right-circled aSaison"></button></li>');				
 			}
 		}
 	}; // On affiche tout les titres des séries ajoutée par l'utilisateur
@@ -102,46 +106,36 @@
 			bMenu = false;
 			$sideMenu.animate({
 				    left: '-=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
 			$content.animate({
 				    left: '-=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
 			$nav.animate({
 				    left: '-=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
 			$searchBox.animate({
 				    left: '-=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
+			$infosBox.animate({
+					left: '-=75%'
+				  }, 1000);
 		}else{
 			bMenu = true;
 			$sideMenu.animate({
 				    left: '+=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
 			$content.animate({
 				    left: '+=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
 			$nav.animate({
 				    left: '+=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
 			$searchBox.animate({
 				    left: '+=75%'
-				  }, 1000, function() {
-				  	//effectuer
-				  });
+				  }, 1000);
+			$infosBox.animate({
+					left: '+=75%'
+				  }, 1000);
 		}
 	}; // On anime l'interface
 
@@ -151,26 +145,24 @@
 			bSearch = false;
 			$content.animate({
 				    marginTop: '-=2em'
-				  }, 500, function() {
-				  	//effectuer
-				  });
+				  }, 500);
 			$searchBox.animate({
 				    top: '-=2em'
-				  }, 500, function() {
-				  	//effectuer
-				  });
+				  }, 500);
+			$infosBox.animate({
+				    top: '-=2em'
+				  }, 500);
 		}else{
 			bSearch = true;
 			$content.animate({
 				    marginTop: '+=2em'
-				  }, 500, function() {
-				  	//effectuer
-				  });
+				  }, 500);
 			$searchBox.animate({
 				    top: '+=2em'
-				  }, 500, function() {
-				  	//effectuer
-				  });
+				  }, 500);
+			$infosBox.animate({
+				    top: '+=2em'
+				  }, 500);
 		}
 	}; // On montre ou as la zone de recherche
 
@@ -197,17 +189,13 @@
 	var addSerie = function(event){
 		var objetThis = $(this);
 			
-		getInfoSerie($(this).parent().attr("data-url"), function(e){
+		getInfoSerie(objetThis.parent().attr("data-url"), function(e){
 			var data = {},
 				laSerie = {"url": objetThis.parent().attr("data-url"),
 				   	   "title" : objetThis.parent().attr("data-titre")
 				      };
 
 			data.banner = e.root.show.banner;
-			data.description = e.root.show.description;
-			data.genres = e.root.show.genres;
-			data.id_thetvdb = e.root.show.id_thetvdb;
-			data.seasons = e.root.show.seasons;
 			laSerie.infosSerie = data;
 			window.localStorage.setItem("serie_" + objetThis.parent().attr("data-titre"), JSON.stringify(laSerie));
 		});
@@ -234,17 +222,113 @@
 		var objetThis = $(this);
 		$content.animate({
 				    left: '-=100%'
-				  }, 1000, function() {
-				  	//effectuer
+				  }, 1000);
+		$infosBox.animate({
+				    left: '-=100%'
+				  }, 1000, function(){
+				  	$content.hide();
 				  });
 		
 		getInfoSerie(objetThis.parent().attr("data-url"), function(e){
 			console.log(e);
+			$infosBox.attr('data-url', e.root.show.url);
+			$infosBox.attr('data-titre', e.root.show.title);
 			$("#infosSerie img").attr('src', e.root.show.banner);
 			$("#infosSerie h1").text(e.root.show.title);
 			$(".description").text(e.root.show.description);
+			$(".genre p").text(e.root.show.genres);
+			//console.log(e.root.show.seasons.length());
+			$(".saisons p").text(calculateNumberOfSeasons(e.root.show.seasons));
 		});
 	}; //On affiche mes infos d'une série
+
+	var calculateNumberOfSeasons = function(data){
+		var count = 0;
+
+	    for(var prop in data) {
+	    	if(data.hasOwnProperty(prop))
+	    		++count;
+	    }
+
+	    return count;
+	}; // On calcule le nombre de saison
+
+	var displaySearch = function(e){
+		$content.show();
+		$content.animate({
+				    left: '+=100%'
+				  }, 1000);
+		$infosBox.animate({
+				    left: '+=100%'
+				  }, 1000, function(){
+				  	$("html").scrollTop(0);
+				  });
+	}; // On retourne à la recherche
+
+	var getDataSerie = function(urlSerie, successCallback){
+		$.ajax(
+				{
+					url:"http://api.betaseries.com/shows/episodes/" + urlSerie + ".json?summary=1&hide_notes=1&key=81e3d2922ed3",
+					type:"get",
+					dataType: "jsonp",
+					success:function(e){			
+						successCallback.apply(null, [e]);
+					},
+					error:function(e,f,g){
+						$listeDesSeries.append('<li class="errors">Une erreur s\'est produite lors du traitement de la requête</li>');
+					}
+				}
+			)
+	};
+
+	var afficheSaison = function(e){
+		var objetThis = $(this);
+		
+		$content.animate({
+				    left: '-=100%'
+				  }, 1000);
+		$maSerie.animate({
+				    left: '-=100%'
+				  }, 1000, function(){
+				  	$content.hide();
+				  });
+
+		getDataSerie(objetThis.parent().attr("data-url"), function(e){
+			console.log(e);
+			console.log(e.root.seasons.length);	
+
+			for(var i = 0; i<e.root.seasons.length; i++){
+				$maSerie.append('<h2>Saison ' + (parseInt(i)+parseInt(1)) + '</h2>');
+
+				for(var j = 0; j<e.root.seasons[i].episodes.length; j++){
+					var nb = (parseInt(j)+parseInt(1));
+					$maSerie.append('<p>Episode ' + nb.toString() + ' : ' + e.root.seasons[i].episodes[j].title + '</p>');
+				}
+			}
+		});
+	};
+
+	var getDataPlanning = function(successCallback){
+		$.ajax(
+				{
+					url:"http://api.betaseries.com/planning/general.json?key=81e3d2922ed3",
+					type:"get",
+					dataType: "jsonp",
+					success:function(e){			
+						successCallback.apply(null, [e]);
+					},
+					error:function(e,f,g){
+						$listeDesSeries.append('<li class="errors">Une erreur s\'est produite lors du traitement de la requête</li>');
+					}
+				}
+			)
+	};
+
+	var afficherMonPlanning = function(e){
+		getDataPlanning(function(e){
+			console.log(e);
+		});
+	};
 
 	$( function () {
 		// --- onload routines
@@ -258,7 +342,12 @@
 		$toAdd.on("click", goToAddPage);
 		$(".icon-plus-circled").live("click", addSerie);
 		$(".icon-info-circled").live("click", displayInfoSerie);
+		$(".icon-back").on("click", displaySearch);
+		$("#infosSerie.icon-plus-circled").on("click", addSerie);
 		$("#toList").on("click", listSeries);
+		$(".aSaison").live("click", afficheSaison);
+		$toPlan.on("click", afficherMonPlanning);
+
 		console.log(window.localStorage);
 	} );
 
